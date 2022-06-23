04e77d6b6ac3466ec53328d6fa95bc953fe025b2
CHAMN13 <107508309+CHAMN13@users.noreply.github.com>
2022-06-22 21:23:53 -0500

    es12

---
+++ src/es12/index.js
@@ -0,0 +1,54 @@
+// Replace all
+const string = "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web";
+const replacedString = string.replace("JavaScript", "Python");
+console.log(replacedString)
+
+const replacedString2 = string.replaceAll("JavaScript", "Python");
+console.log(replacedString2);
+
+
+// Metodos privados
+class Message {
+  #show(val) {
+    console.log(val);
+  };
+  get #add(val) [
+    ...
+  ]
+}
+
+const message = new Message();
+message.show('Hola!');
+
+
+// Promise any
+const promise1 = new Promise((resolve, reject) => reject("1"));
+const promise2 = new Promise((resolve, reject) => resolve("2"));
+const promise3 = new Promise((resolve, reject) => resolve("3"));
+
+Promise.any([promise1, promise2, promise3])
+  .then(response => console.log(response));
+
+
+
+// WeakRef
+class AnyClass {
+  constructor(element){
+      this.ref = new WeakRef
+  }
+  {...}
+}
+
+
+// Nuevos operadores lógicos
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue &&= isFalse); // false
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue ||= isFalse); // true
+
+let isTrue = undefined;
+let isFalse = false;
+console.log(isTrue ??= isFalse); // false
